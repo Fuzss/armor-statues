d2d935d1049bbd11c3dbeb7c2a5665fcd91645b0
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-05-08 00:22:18 +0200

    remove outdated versions

--- 1.19.2/Common/src/main/java/fuzs/armorstatues/proxy/Proxy.java
+++
@@ -1,9 +0,0 @@
-package fuzs.armorstatues.proxy;
-
-import net.minecraft.world.entity.decoration.ArmorStand;
-import net.minecraft.world.entity.player.Player;
-
-public interface Proxy {
-
-    void openArmorStandScreen(ArmorStand armorStand, Player player);
-}
